e74d6ac29bd436247491f0beea3a163eaf8ac75b
Adam Barcewicz <barcel91@gmail.com>
2026-05-31 10:25:27 +0200

    creted classes

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+public class App {
+
+    // główna klasa aplikacji z main
+    //zarządzanie
+    //czytanie argumentów
+    //wybieranie statystyki i wypisanie na ekran
+
+
+}
---
+++ src/main/java/DataLoader.java
@@ -0,0 +1,5 @@
+public class DataLoader {
+
+    // przyjmuje plik JSON
+    // tworzy jakąś listę "Pitstop"
+}
