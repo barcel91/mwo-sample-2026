424c4ab5e6babf68f72c81faed211372c6ea21c9
gomolkap <pgomolka@student.agh.edu.pl>
2026-05-31 12:20:14 +0200

    test commit 2

---
+++ src/main/java/pl/edu/agh/mwo/PitStopCounter.java
@@ -0,0 +1,10 @@
+import pl.edu.agh.mwo.PitStop;
+
+import java.util.List;
+
+public class PitStopCounter {
+
+    public int countPitStops(List<PitStop> pitStops) {
+        return pitStops.size();
+    }
+}
---
+++ src/test/java/pl/edu/agh/mwo/PitStopCounterTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+import pl.edu.agh.mwo.PitStop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PitStopCounterTest {
+
+    @Test
+    void countPitStops() {
+        List<PitStop> pitStops = new ArrayList<>();
+        pitStops.add(new PitStop(31.3, 7, 14));
+        pitStops.add(new PitStop(30.0, 8, 23));
+
+        PitStopCounter pitStopCounter = new PitStopCounter();
+
+        int pitStopsNumber = pitStopCounter.countPitStops(pitStops);
+
+        assertEquals(2, pitStopsNumber);
+
+    }
+}
